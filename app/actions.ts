0a6e0e7c6e08d97e8da97cfb1f8ce7b4909d6c27
ahmadrosid <alahmadrosid@gmail.com>
2025-03-17 23:53:58 +0700

    Tidy Up

--- app/actions.ts
+++ app/actions.ts
@@ -7,9 +7,6 @@ export async function handleVideoSubmit(formData: FormData) {
   const videoUrl = formData.get("videoUrl") as string
   if (!videoUrl) return
 
-  // add delay for 5 seconds
-  await new Promise((resolve) => setTimeout(resolve, 5000))
-
   const videoId = extractVideoId(videoUrl)
   if (!videoId) return
 
