c399f2471f870fac817fe1669cd8c93da4f42154
ahmadrosid <alahmadrosid@gmail.com>
2025-06-19 01:59:16 +0700

    Tidy Up

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -2,8 +2,6 @@
 
 import { authClient } from "@/lib/auth-client";
 import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
 import Link from 'next/link'
 
 export default function LoginPage() {
