6d86264ee145ff975bc879d844d9e34b01039057
ahmadrosid <alahmadrosid@gmail.com>
2025-06-19 10:10:56 +0700

    Tidy Up

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from 'drizzle-kit';
 import { env } from './lib/env/server';
 
-console.log('env.DATABASE_URL', env.DATABASE_URL);
-
 export default defineConfig({
   schema: './lib/db/schema/*.ts',
   out: './drizzle',
