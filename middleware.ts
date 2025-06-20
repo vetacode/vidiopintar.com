e5be4f26456438d4ee2bfc706e75bc74d948c82c
ahmadrosid <alahmadrosid@gmail.com>
2025-06-20 13:59:20 +0700

    Update CORS origin to specific domain vidiopintar.com

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 export async function middleware(request: NextRequest) {
     const response = NextResponse.next();
 
-    response.headers.set('Access-Control-Allow-Origin', '*'); // or specify origin
+    response.headers.set('Access-Control-Allow-Origin', 'https://vidiopintar.com');
     response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   
