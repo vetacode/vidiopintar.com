7c423a5aca7e5ac86904131c3fda0c19f1575c2f
ahmadrosid <alahmadrosid@gmail.com>
2025-06-20 11:37:26 +0700

    Tidy Up

--- components/submit-button.tsx
+++ components/submit-button.tsx
@@ -7,7 +7,6 @@ import { cn } from "@/lib/utils";
 
 export const SubmitButton = forwardRef<HTMLButtonElement, { label: string, className?: string }>(( { label, className }, ref ) => {
     const { pending } = useFormStatus();
-    console.log({ pending });
     return (
         <Button ref={ref} type="submit" className={cn('rounded-xl cursor-pointer', className)} disabled={pending} aria-disabled={pending}>
             {pending ? <LoadingSpinner text="Loading..." /> : label}
