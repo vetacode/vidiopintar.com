64b70923b0786bd2584431950d1236f44d23b277
ahmadrosid <alahmadrosid@gmail.com>
2025-06-21 02:35:50 +0700

    Tidy Up

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,5 +1,5 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-    baseURL: window ? window.location.origin : process.env.NEXT_PUBLIC_SITE_URL,
+    baseURL: typeof window !== 'undefined' ? window.location.origin : process.env.NEXT_PUBLIC_SITE_URL,
 });
