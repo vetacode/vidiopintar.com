8291c00f5bb57a8c84acfebe4f654bd5995f45fc
ahmadrosid <alahmadrosid@gmail.com>
2025-06-21 02:39:39 +0700

    Tidy Up

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ['i.ytimg.com','res.cloudinary.com'],
   },
+  trustedOrigins: [
+    "https://vidiopintar.com",
+    "https://www.vidiopintar.com"
+  ]
 }
 
 module.exports = nextConfig
