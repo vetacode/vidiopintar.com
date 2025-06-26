badf0df6089f932cab521f503aa2df8539c021b1
ahmadrosid <alahmadrosid@gmail.com>
2025-06-26 12:28:47 +0700

    Tidy Up

--- src/app/watch/page.tsx
+++ src/app/watch/page.tsx
@@ -2,19 +2,12 @@ import { redirect } from 'next/navigation';
 import { Suspense } from 'react';
 import { LoadingSpinner } from '@/components/ui/loading-spinner';
 import { extractVideoId } from '@/lib/utils';
-import { processVideo } from '@/app/actions';
 
 async function ProcessAndRedirect({ videoUrl }: { videoUrl: string }) {
   const videoId = extractVideoId(videoUrl);
   if (!videoId) {
     return <div>Invalid video URL provided.</div>;
   }
-
-  // await new Promise((resolve) => setTimeout(resolve, 2000));
-  // throw new Error('Failed to process video');
-
-  await processVideo(videoId);
-
   redirect(`/video/${videoId}`);
 
   return null;
