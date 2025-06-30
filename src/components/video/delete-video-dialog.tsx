001a603f8c0dd52527a96e1274dd84a0f0563ca4
ahmadrosid <alahmadrosid@gmail.com>
2025-07-01 00:17:48 +0700

    fix delete video button

--- src/components/video/delete-video-dialog.tsx
+++ src/components/video/delete-video-dialog.tsx
@@ -18,10 +18,19 @@ import { useFormStatus } from "react-dom";
 import { Button } from "@/components/ui/button";
 import { Loader } from "lucide-react";
 
+function DeleteButton() {
+    const { pending } = useFormStatus();
+    
+    return (
+        <Button type="submit" disabled={pending} className="cursor-pointer">
+            {pending ? <Loader className="size-4 animate-spin" /> : "Yes, delete!"}
+        </Button>
+    );
+}
+
 export function DeleteVideoDialog() {
     const { isOpen, closeDialog, id } = useDeleteVideoDialogStore()
     const [state, formAction] = useFormState(handleDeleteVideo, { success: false, errors: undefined });
-    const { pending } = useFormStatus();
 
     useEffect(() => {
         if (state.success) {
@@ -50,9 +59,7 @@ export function DeleteVideoDialog() {
                     <AlertDialogFooter>
                         <AlertDialogCancel type="button">Cancel</AlertDialogCancel>
                         <AlertDialogAction asChild>
-                    <Button type="submit" disabled={pending} className="cursor-pointer">
-                        {pending ? <Loader className="size-4 animate-spin" /> : "Yes, delete!"}
-                    </Button>
+                            <DeleteButton />
                         </AlertDialogAction>
                     </AlertDialogFooter>
                 </form>
