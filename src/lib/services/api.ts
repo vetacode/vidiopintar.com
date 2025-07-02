478bc5420d14b3e9fc43ce00b52df0e4ad27b547
ahmadrosid <alahmadrosid@gmail.com>
2025-07-02 23:50:11 +0700

    Improve effect ts for api

--- src/lib/services/api.ts
+++ src/lib/services/api.ts
@@ -1,46 +1,82 @@
-import { FetchHttpClient, HttpClient, HttpClientRequest, HttpClientResponse } from "@effect/platform";
-import { Effect, flow } from "effect";
-import { ShareChatRequest, ShareChatResponse, ClearMessagesRequest, ClearMessagesResponse, VideoSearchRequest, VideoSearchResponse } from "@/lib/services/schema";
+import {
+    FetchHttpClient,
+    HttpClient,
+    HttpClientRequest,
+    HttpClientResponse,
+} from "@effect/platform";
+import { Effect, Schedule, Schema } from "effect";
+import {
+    ShareChatRequest,
+    ShareChatResponse,
+    ClearMessagesRequest,
+    ClearMessagesResponse,
+    VideoSearchResponse,
+} from "@/lib/services/schema";
 
 export class Api extends Effect.Service<Api>()("Api", {
     dependencies: [FetchHttpClient.layer],
+    accessors: true,
     effect: Effect.gen(function* () {
         const baseClient = yield* HttpClient.HttpClient;
+
         const client = baseClient.pipe(
-            HttpClient.mapRequest(
-                flow(
-                    HttpClientRequest.prependUrl(`/api`),
-                    HttpClientRequest.acceptJson
-                )
-            )
-        );
-        
-        const externalClient = baseClient.pipe(
-            HttpClient.mapRequest(HttpClientRequest.acceptJson)
+            HttpClient.mapRequest(HttpClientRequest.prependUrl(`/api`)),
+            HttpClient.mapRequest(HttpClientRequest.acceptJson),
+            HttpClient.retryTransient(Schedule.exponential("100 millis"))
         );
 
+        const externalClient = baseClient.pipe(
+            HttpClient.mapRequest(HttpClientRequest.acceptJson),
+            HttpClient.retryTransient(Schedule.exponential("100 millis"))
+        );
+
+        const post =
+            <AReq, IReq, RReq, ARes, IRes, RRes>({
+                url,
+                req,
+                res,
+            }: {
+                url: string;
+                req: Schema.Schema<AReq, IReq, RReq>;
+                res: Schema.Schema<ARes, IRes, RRes>;
+            }) =>
+                (body: AReq) =>
+                    HttpClientRequest.post(url).pipe(
+                        HttpClientRequest.schemaBodyJson(req)(body),
+                        Effect.flatMap(client.execute),
+                        Effect.flatMap(HttpClientResponse.schemaBodyJson(res))
+                    );
+
+        const externalGet = <ARes, IRes, RRes>({
+            url,
+            res,
+        }: {
+            url: string;
+            res: Schema.Schema<ARes, IRes, RRes>;
+        }) =>
+            HttpClientRequest.get(url).pipe(
+                externalClient.execute,
+                Effect.flatMap(HttpClientResponse.schemaBodyJson(res))
+            );
+
         return {
-            createShareVideo: (body: ShareChatRequest) =>
-                HttpClientRequest.schemaBodyJson(ShareChatRequest)
-                (HttpClientRequest.post("/share"), body).pipe(
-                    Effect.flatMap(client.execute),
-                    Effect.flatMap(HttpClientResponse.schemaBodyJson(ShareChatResponse)),
-                    Effect.scoped
-                ),
-            clearMessages: (body: ClearMessagesRequest) =>
-                HttpClientRequest.schemaBodyJson(ClearMessagesRequest)
-                (HttpClientRequest.post("/clear-messages"), body).pipe(
-                    Effect.flatMap(client.execute),
-                    Effect.flatMap(HttpClientResponse.schemaBodyJson(ClearMessagesResponse)),
-                    Effect.scoped
-                ),
-            searchVideos: (query: string) =>
-                externalClient.execute(
-                    HttpClientRequest.get(`https://api.ahmadrosid.com/youtube/search?q=${encodeURIComponent(query)}`)
-                ).pipe(
-                    Effect.flatMap(HttpClientResponse.schemaBodyJson(VideoSearchResponse)),
-                    Effect.scoped
-                )
+            createShareVideo: Effect.fn("createShareVideo")(post({
+                url: "/share",
+                req: ShareChatRequest,
+                res: ShareChatResponse,
+            })),
+            clearMessages: Effect.fn("clearMessages")(post({
+                url: "/clear-messages",
+                req: ClearMessagesRequest,
+                res: ClearMessagesResponse,
+            })),
+            searchVideos: Effect.fn("searchVideos")((query: string) =>
+                externalGet({
+                    url: `https://api.ahmadrosid.com/youtube/search?q=${encodeURIComponent(
+                        query
+                    )}`,
+                    res: VideoSearchResponse,
+                })),
         };
     }),
 }) { }
