f9387aed7ea73ddb3c21a03a3ac435bd39ccbe11
ahmadrosid <alahmadrosid@gmail.com>
2025-07-13 15:24:20 +0700

    Add pricing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { HeroSection } from "@/components/landing/hero-section";
 import { HeroHeader } from "@/components/hero-header";
 import { Features } from "@/components/landing/features";
+import { Pricing } from "@/components/landing/pricing";
 import { WallOfLoveSection } from "@/components/landing/testimonials";
 import { FooterSection } from "@/components/footer";
 import { CallToAction } from "@/components/landing/call-to-action";
@@ -13,6 +14,7 @@ export default function Page() {
       <HeroHeader />
       <HeroSection />
       <Features />
+      <Pricing />
       <WallOfLoveSection />
       <CallToAction />
       <FooterSection />
